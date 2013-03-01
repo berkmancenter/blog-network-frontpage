d820cb9ba8ba81d927badfd5dd2717fd4070ba63
Cliff Song <csong@cyber.law.harvard.edu>
2013-03-01 16:19:53 -0500

    Added login pop-up functionality.

--- header.php
+++ header.php
@@ -46,7 +46,36 @@
 		</nav><!-- #site-navigation -->
 
 		<nav class="nav-right">
-			<button type="button">Sign In</button>
+			<?php global $current_user, $blog_id;
+			if (!is_user_logged_in()) { ?>
+				<a href="#login" class="fancybox">Login</a>
+				<div class="fancybox-hidden" style="display:none">
+					<div id="login" style="width:300px;height:200px">
+						<form name="loginform" id="loginform" action="wp-login.php" method="post">
+						<h2>Already a blogger?</h2>
+							<ul>
+								<li><strong>Username:</strong></li>
+								<li><input type="text" name="log" id="log" value="" size="20" tabindex="1" /></li>
+								<li><strong>Password:</strong></li>
+								<li><input type="password" name="pwd" id="pwd" value="" size="20" tabindex="2" /></li>
+								<li><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="3" /> Remember me</li>
+								<li style="text-align: right; margin-right: 8px"><input type="submit" name="submit" id="submitform" value="Login &raquo;" tabindex="4" /></li>
+								<input type="hidden" name="redirect_to" value="'<?php echo $_SERVER["REQUEST_URI"] ?>'" />
+							</ul>
+						</form>
+					</div>
+				</div>
+			<?php
+			} 
+			else {
+				$blogs = get_blogs_of_user($current_user->ID);
+				$username = $current_user->user_login;
+			?>
+				<h2>Hi <?php echo $username; ?>! <span class="logout">(<a href="<?php echo wp_logout_url($_SERVER["REQUEST_URI"]) ?>">Logout</a>)</h2>
+			<?php
+			}
+			?> 
+
 			<input type=button onClick="parent.location='<?php echo site_url('wp-signup.php','login') ?>'" value='Register'>
 						
 			<?php get_search_form(true); ?>
